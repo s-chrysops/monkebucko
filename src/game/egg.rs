b158ab78c2c8c7c69dcfb1f75b52cb198b66c574
s-chrysops <st.chrysops@gmail.com>
2025-07-18 12:32:15 -0700

    refactor(egg): rework stars systems

--- src/game/egg.rs
+++ src/game/egg.rs
@@ -62,7 +62,7 @@ pub fn egg_plugin(app: &mut App) {
         (
             spawn_player,
             spawn_world,
-            spawn_stars,
+            setup_stars,
             spawn_cracking_elements,
             setup_crt,
             cursor_grab,
@@ -77,7 +77,8 @@ pub fn egg_plugin(app: &mut App) {
     .add_systems(
         Update,
         (
-            move_stars,
+            update_stars_position,
+            despawn_respawn_stars,
             (
                 move_player,
                 get_egg_interactions
@@ -110,8 +111,7 @@ pub fn egg_plugin(app: &mut App) {
         ),
     )
     .add_sub_state::<EggState>()
-    .add_sub_state::<CrackingPhase>()
-    .init_resource::<StarResources>();
+    .add_sub_state::<CrackingPhase>();
 }
 
 fn wait_till_loaded(
@@ -894,53 +894,46 @@ fn spawn_cracking_elements(mut commands: Commands, asset_server: Res<AssetServer
     }
 }
 
+#[derive(Debug, Component)]
+struct StarRoot;
+
+#[derive(Debug, Component)]
+// Star with parallax speed
+struct Star {
+    speed: f32,
+}
+
 const MAX_STAR_AMOUNT: usize = 300;
 const BACK_STAR_AMOUNT: usize = 200;
-const MIN_STAR_HEIGHT: f32 = -15.0;
-const MAX_STAR_HEIGHT: f32 = 30.0;
-const LUMINANCE_LEVELS: usize = 4;
-const MIN_STAR_LUMINANCE: f32 = 4.0;
-const MAX_STAR_LUMINANCE: f32 = 400.0;
+
 const MIN_STAR_SPEED: f32 = 0.01;
 const MAX_STAR_SPEED: f32 = 0.5;
 
-// Star with parallax speed
-#[derive(Debug, Component)]
-struct Star(f32);
+const MIN_STAR_HEIGHT: f32 = -15.0;
+const MAX_STAR_HEIGHT: f32 = 30.0;
 
-// Star mesh and materials with 4 levels of luminance
-#[derive(Debug, Resource)]
+const LUMINANCE_LEVELS: usize = 4;
+const MIN_STAR_LUMINANCE: f32 = 4.0;
+const MAX_STAR_LUMINANCE: f32 = 400.0;
+
+#[derive(Debug, Component)]
+// Star mesh and materials for each levels of luminance
 struct StarResources {
     mesh:      Handle<Mesh>,
     materials: [Handle<StandardMaterial>; LUMINANCE_LEVELS],
 }
 
-impl FromWorld for StarResources {
-    fn from_world(world: &mut World) -> Self {
-        let mut meshes = world.resource_mut::<Assets<Mesh>>();
-        let mesh = meshes.add(Circle::new(0.01));
-
-        let mut materials = world.resource_mut::<Assets<StandardMaterial>>();
-        let lum_increment = (MAX_STAR_LUMINANCE - MIN_STAR_LUMINANCE) / LUMINANCE_LEVELS as f32;
-        let materials = (0..LUMINANCE_LEVELS)
-            .map(|level| {
-                let lum = lum_increment * level as f32;
-                materials.add(StandardMaterial {
-                    emissive: LinearRgba::rgb(lum, lum, lum),
-                    ..default()
-                })
-            })
-            .collect::<Vec<Handle<StandardMaterial>>>()
-            .try_into()
-            .unwrap();
-
-        StarResources { mesh, materials }
-    }
+struct StarInfo {
+    speed:     f32,
+    lum_level: usize,
+    transform: Transform,
 }
 
-fn generate_star(rng: &mut Entropy<WyRand>, count: usize) -> (f32, usize, Transform) {
+fn generate_star(rng: &mut Entropy<WyRand>, count: usize) -> StarInfo {
+    const STARBOX_RADIUS: f32 = 10.0;
+
     // Star's angle on the half-cylinder skybox
-    let angle = random_range(rng, 0.0, std::f32::consts::PI);
+    let angle = random_range(rng, 0.0, PI);
 
     // Static stars
     let speed = match count < BACK_STAR_AMOUNT {
@@ -954,56 +947,106 @@ fn generate_star(rng: &mut Entropy<WyRand>, count: usize) -> (f32, usize, Transf
         false => MAX_STAR_HEIGHT,
     };
 
-    let lum = rng.next_u32() as usize % 4;
+    let lum_level = rng.next_u32() as usize % 4;
 
-    let mut transform = Transform::from_xyz(10.0 * ops::cos(angle), height, 10.0 * ops::sin(angle));
-    transform.rotate_local_y(3.0 * std::f32::consts::FRAC_PI_2 - angle);
+    let transform = Transform::from_xyz(
+        STARBOX_RADIUS * angle.cos(),
+        height,
+        STARBOX_RADIUS * angle.sin(),
+    )
+    .with_rotation(Quat::from_rotation_y(0.75 * TAU - angle));
 
-    (speed, lum, transform)
+    StarInfo {
+        speed,
+        lum_level,
+        transform,
+    }
 }
 
-fn spawn_stars(
+fn setup_stars(
     mut commands: Commands,
     mut rng: GlobalEntropy<WyRand>,
-    resources: Res<StarResources>,
+    asset_server: Res<AssetServer>,
 ) {
     info!("Spawning stars");
-    let initial_stars: Vec<_> = (0..MAX_STAR_AMOUNT)
-        .map(|i| generate_star(&mut rng, i))
-        .map(|(speed, lum, transform)| {
-            (
-                OnEggScene,
-                Star(speed),
-                Mesh3d(resources.mesh.clone_weak()),
-                MeshMaterial3d(resources.materials[lum].clone_weak()),
-                transform,
-            )
+
+    let lum_increment = (MAX_STAR_LUMINANCE - MIN_STAR_LUMINANCE) / LUMINANCE_LEVELS as f32;
+    let resources = StarResources {
+        mesh:      asset_server.add(Circle::new(0.01).into()),
+        materials: (0..LUMINANCE_LEVELS)
+            .map(|level| {
+                let lum = lum_increment * level as f32;
+                asset_server.add(StandardMaterial {
+                    emissive: LinearRgba::rgb(lum, lum, lum),
+                    ..default()
+                })
+            })
+            .collect::<Vec<Handle<StandardMaterial>>>()
+            .try_into()
+            .unwrap(),
+    };
+
+    commands
+        .spawn((
+            OnEggScene,
+            StarRoot,
+            Name::new("Stars"),
+            Transform::default(),
+            Visibility::default(),
+        ))
+        .with_children(|parent| {
+            (0..MAX_STAR_AMOUNT)
+                .map(|i| generate_star(&mut rng, i))
+                .for_each(|info| {
+                    let StarInfo {
+                        speed,
+                        lum_level,
+                        transform,
+                    } = info;
+                    parent.spawn((
+                        Star { speed },
+                        Mesh3d(resources.mesh.clone_weak()),
+                        MeshMaterial3d(resources.materials[lum_level].clone_weak()),
+                        transform,
+                    ));
+                })
         })
-        .collect();
-    commands.spawn_batch(initial_stars);
+        .insert(resources);
 }
 
-fn move_stars(
-    mut commands: Commands,
-    stars: Query<(Entity, &Star, &mut Transform)>,
-    mut rng: GlobalEntropy<WyRand>,
-    resources: Res<StarResources>,
-) {
+fn update_stars_position(mut stars: Query<(&Star, &mut Transform)>) {
     stars
-        .into_iter()
-        .for_each(|(entity, Star(speed), mut transform)| {
+        .iter_mut()
+        .for_each(|(Star { speed }, mut transform)| {
             transform.translation.y -= speed;
-            if transform.translation.y <= MIN_STAR_HEIGHT {
-                commands.entity(entity).despawn();
-                let (speed, lum, transform) = generate_star(&mut rng, MAX_STAR_AMOUNT);
-                commands.spawn((
-                    OnEggScene,
-                    Star(speed),
-                    Mesh3d(resources.mesh.clone_weak()),
-                    MeshMaterial3d(resources.materials[lum].clone_weak()),
-                    transform,
-                ));
-            }
+        });
+}
+
+fn despawn_respawn_stars(
+    stars: Query<(Entity, &Transform), With<Star>>,
+    star_root: Single<(Entity, &StarResources), With<StarRoot>>,
+    mut rng: GlobalEntropy<WyRand>,
+    mut commands: Commands,
+) {
+    let (star_root, resources) = star_root.into_inner();
+    stars
+        .iter()
+        .filter_map(|(entity, transform)| {
+            (transform.translation.y <= MIN_STAR_HEIGHT).then_some(entity)
+        })
+        .for_each(|entity| {
+            commands.entity(entity).despawn();
+            let StarInfo {
+                speed,
+                lum_level,
+                transform,
+            } = generate_star(&mut rng, MAX_STAR_AMOUNT);
+            commands.entity(star_root).with_child((
+                Star { speed },
+                Mesh3d(resources.mesh.clone_weak()),
+                MeshMaterial3d(resources.materials[lum_level].clone_weak()),
+                transform,
+            ));
         });
 }
 
