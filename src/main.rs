d745ed3290e3917148b881592b0d52a5653f3c6a
s-chrysops <st.chrysops@gmail.com>
2025-07-20 09:27:41 -0700

    build(alpha): add profiles and feature flags for dev dependencies

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,117 @@ mod auto_scaling;
 const WINDOW_WIDTH: f32 = 1280.0;
 const WINDOW_HEIGHT: f32 = 720.0;
 
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum AppState {
+    #[default]
+    Splash,
+    Menu,
+    Game,
+}
+
+fn main() {
+    let mut app = App::new();
+
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                    prevent_default_event_handling: false,
+                    ..default()
+                }),
+                ..default()
+            })
+            // .set(PickingPlugin {
+            //     is_window_picking_enabled: false,
+            //     ..default()
+            // })
+            .set(ImagePlugin::default_nearest()),
+    );
+
+    #[cfg(feature = "debug-pickings")]
+    app.add_plugins(bevy::dev_tools::picking_debug::DebugPickingPlugin)
+        .insert_resource(bevy::dev_tools::picking_debug::DebugPickingMode::Normal);
+
+    #[cfg(feature = "debug-physics")]
+    app.add_plugins(avian2d::prelude::PhysicsDebugPlugin::default());
+
+    #[cfg(feature = "inspector")]
+    app.add_plugins((
+        bevy_inspector_egui::bevy_egui::EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        },
+        bevy_inspector_egui::quick::WorldInspectorPlugin::new(),
+    ));
+
+    #[cfg(feature = "export-types")]
+    app.add_plugins(TiledMapPlugin(TiledMapPluginConfig::default()));
+
+    #[cfg(not(feature = "export-types"))]
+    app.add_plugins(TiledMapPlugin(TiledMapPluginConfig {
+        tiled_types_export_file: None,
+    }));
+
+    app.add_plugins((
+        MeshPickingPlugin,
+        TextAnimatorPlugin,
+        ScalePlugin,
+        TiledPhysicsPlugin::<TiledPhysicsAvianBackend>::default(),
+        PhysicsPlugins::default().with_length_unit(32.0),
+        EntropyPlugin::<WyRand>::default(),
+    ))
+    .add_plugins((
+        game_plugin,
+        menu_plugin,
+        splash_plugin,
+        sprite_animations_plugin,
+        progress_plugin,
+    ))
+    .init_asset::<Blob>()
+    .init_asset_loader::<BlobAssetLoader>()
+    .init_state::<AppState>()
+    .add_systems(Startup, (setup_overlay_camera, initialize_settings))
+    .insert_resource(avian2d::prelude::Gravity::ZERO)
+    .insert_resource(MeshPickingSettings {
+        require_markers:     true,
+        ray_cast_visibility: RayCastVisibility::VisibleInView,
+    })
+    .run();
+}
+
 const RENDER_LAYER_WORLD: RenderLayers = RenderLayers::layer(0);
 const RENDER_LAYER_OVERLAY: RenderLayers = RenderLayers::layer(1);
 const RENDER_LAYER_SPECIAL: RenderLayers = RenderLayers::layer(2);
 
 #[derive(Debug, Component)]
 struct OverlayCamera;
+
+fn setup_overlay_camera(mut commands: Commands) {
+    use bevy::render::camera::ScalingMode;
+    commands.spawn((
+        Camera2d,
+        Camera {
+            order: 1,
+            clear_color: ClearColorConfig::Custom(Color::NONE),
+            output_mode: CameraOutputMode::Write {
+                blend_state: Some(BlendState::ALPHA_BLENDING),
+                clear_color: ClearColorConfig::None,
+            },
+            ..default()
+        },
+        auto_scaling::AspectRatio(16.0 / 9.0),
+        Projection::from(OrthographicProjection {
+            near: -1000.0,
+            scaling_mode: ScalingMode::Fixed {
+                width:  WINDOW_WIDTH,
+                height: WINDOW_HEIGHT,
+            },
+            ..OrthographicProjection::default_3d()
+        }),
+        // Msaa::Off,
+        RENDER_LAYER_OVERLAY,
+    ));
+}
 
 #[derive(Debug, Resource, Serialize, Deserialize)]
 struct Settings {
@@ -69,96 +174,6 @@ impl Default for Settings {
             music_vol: 10,
         }
     }
-}
-
-#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
-enum AppState {
-    #[default]
-    Splash,
-    Menu,
-    Game,
-}
-
-fn main() {
-    let mut app = App::new();
-    app.add_plugins((
-        DefaultPlugins
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-                    prevent_default_event_handling: false,
-                    ..default()
-                }),
-                ..default()
-            })
-            // .set(PickingPlugin {
-            //     is_window_picking_enabled: false,
-            //     ..default()
-            // })
-            .set(ImagePlugin::default_nearest()),
-        MeshPickingPlugin,
-        TextAnimatorPlugin,
-        ScalePlugin,
-        TiledMapPlugin(TiledMapPluginConfig {
-            // Fixes crash on WASM
-            tiled_types_export_file: None,
-            // ..default()
-        }),
-        TiledPhysicsPlugin::<TiledPhysicsAvianBackend>::default(),
-        PhysicsPlugins::default().with_length_unit(32.0),
-        EntropyPlugin::<WyRand>::default(),
-        // bevy::dev_tools::picking_debug::DebugPickingPlugin,
-        // avian2d::prelude::PhysicsDebugPlugin::default(),
-        // bevy_inspector_egui::bevy_egui::EguiPlugin {
-        //     enable_multipass_for_primary_context: true,
-        // },
-        // bevy_inspector_egui::quick::WorldInspectorPlugin::new(),
-    ))
-    .add_plugins((
-        game_plugin,
-        menu_plugin,
-        splash_plugin,
-        sprite_animations_plugin,
-        progress_plugin,
-    ))
-    .init_asset::<Blob>()
-    .init_asset_loader::<BlobAssetLoader>()
-    .init_state::<AppState>()
-    .add_systems(Startup, (setup, initialize_settings))
-    // .insert_resource(DebugPickingMode::Normal)
-    .insert_resource(avian2d::prelude::Gravity::ZERO)
-    .insert_resource(MeshPickingSettings {
-        require_markers:     true,
-        ray_cast_visibility: RayCastVisibility::VisibleInView,
-    })
-    .run();
-}
-
-fn setup(mut commands: Commands) {
-    use bevy::render::camera::ScalingMode;
-    commands.spawn((
-        Camera2d,
-        Camera {
-            order: 1,
-            clear_color: ClearColorConfig::Custom(Color::NONE),
-            output_mode: CameraOutputMode::Write {
-                blend_state: Some(BlendState::ALPHA_BLENDING),
-                clear_color: ClearColorConfig::None,
-            },
-            ..default()
-        },
-        auto_scaling::AspectRatio(16.0 / 9.0),
-        Projection::from(OrthographicProjection {
-            near: -1000.0,
-            scaling_mode: ScalingMode::Fixed {
-                width:  WINDOW_WIDTH,
-                height: WINDOW_HEIGHT,
-            },
-            ..OrthographicProjection::default_3d()
-        }),
-        // Msaa::Off,
-        RENDER_LAYER_OVERLAY,
-    ));
 }
 
 fn initialize_settings(mut commands: Commands) {
